cd2a461de0c70cee49c3968a5a5636bf2b443691
Rohan Prasad <thewarlock321@gmail.com>
2017-09-24 21:44:30 +0530

    Minor Changes.

--- GenerateData.py
+++ GenerateData.py
@@ -1,11 +1,13 @@
 import numpy as np
-import pandas as pd
 import scipy.io
 import os
 import matplotlib.pyplot as plt
 import json
 import argparse
+import multiprocessing
 import warnings
+from matplotlib.ticker import NullLocator
+from matplotlib.pyplot import savefig
 
 parser = argparse.ArgumentParser(
     description="Enter the number of dogs you wish to generate the spectrogram for."
@@ -13,32 +15,69 @@ parser = argparse.ArgumentParser(
 parser.add_argument('num_of_dogs', type=int, default=0, nargs='?')
 args = parser.parse_args()
 
-if args.num_of_dogs == 0:
-    warnings.warn("No argument has been provided. All folders will be processed.")
 
-
-def loadConfig():
+def load_config():
 
     with open('config.json', 'r') as f:
-        config = json.load(f)
+        return json.load(f)
 
-    return config
+config = load_config()
+path = config['path']
+fs = config['parameters']['fs']
+NFFT = config['parameters']['NFFT']
+noverlap = config['parameters']['noverlap']
+electrode = config['electrode']
+output_path = config['output_path']
 
-def loadData():
 
-    config = loadConfig()
-    folders = sorted(os.listdir(config['path']))
-    if (args.num_of_dogs > len(folders)):
+def load_data():
+
+    folders = sorted(os.listdir(path))
+    if args.num_of_dogs > len(folders):
         raise Exception("There aren't that many folders.")
-    matFiles = {}
-    for i in range (0, args.num_of_dogs):
-        matFiles[folders[i]] = sorted(os.listdir(config['path']+folders[i]))
-    print (matFiles.keys())
+    jobs = []
+    k = 0
+    l = 0
+
+    print("Generating Spectrogram for folder: "+folders[args.num_of_dogs - 1])
+
+    for i in range(0, len(os.listdir(path + folders[args.num_of_dogs - 1]))):
+
+        data = scipy.io.loadmat(path + folders[args.num_of_dogs - 1] + '/' +
+                                sorted(os.listdir(path + folders[args.num_of_dogs - 1]))[1:][i])
+        segment = sorted(data.keys(), reverse=True)[0]
+        data = data[segment][0][0][0]
+        if 'interictal' in segment:
+            p = multiprocessing.Process(target=generate_spectrograms,
+                                        args=(data, folders[args.num_of_dogs-1], 'interictal_', k))
+            k += 1
+        elif 'preictal' in segment:
+            p = multiprocessing.Process(target=generate_spectrograms,
+                                        args=(data, folders[args.num_of_dogs - 1], 'preictal_', l))
+            l += 1
+        jobs.append(p)
+        p.start()
+
+
+# generate spectrograms. label 1 => interictal,2 = > preictal
+def generate_spectrograms(data, folder, segment, counter):
+
+    data = np.fft.fft(data[electrode - 1])
+    plt.specgram(data, NFFT=NFFT, Fs=fs, noverlap=noverlap, cmap=plt.cm.jet)
+    gca = plt.gca
+    fig = plt.gcf()
+    fig.set_size_inches(3, 2)
+    gca().set_axis_off()
+    gca().xaxis.set_major_locator(NullLocator())
+    gca().yaxis.set_major_locator(NullLocator())
+    savefig(output_path + folder + '_Spectrograms/' + segment + str(counter) + '.png',
+            bbox_inches='tight', pad_inches=0)
 
 
 def main():
-
-    loadData()
+    if args.num_of_dogs == 0:
+        warnings.warn("No argument has been provided. All folders will be processed.")
+    load_data()
 
 if __name__ == '__main__':
     main()
--- editConfig.py
+++ editConfig.py
@@ -2,16 +2,18 @@ import json
 import os
 
 new_dict = {}
-
+#new_dict['parameters'] = {'fs': 399.6097561, 'noverlap': 512, 'NFFT': 1024}
+#new_dict['output_path'] = '/home/rohan/bioMed/'
 if os.path.isfile('config.json'):
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    if not len(new_dict):
+    if len(new_dict):
         config.update(new_dict)
         with open('config.json', 'w') as f:
             json.dump(config, f)
 
+    print ("Updated 'Config.json': \n", config)
 else:
     config = {'path' : '/media/rohan/My Stuff/EEG/'}
     with open('config.json', 'w') as f:
